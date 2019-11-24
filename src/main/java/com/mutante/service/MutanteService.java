9e57de24f08f19dd9a65d637f480b5c7f3606fd6
ignacioaniball <ignacioaniball@gmail.com>
2019-11-23 21:53:59 -0300

    add method to match row.

--- src/main/java/com/mutante/service/MutanteService.java
+++ src/main/java/com/mutante/service/MutanteService.java
@@ -15,40 +15,44 @@ public class MutanteService {
 
 	@Autowired
 	MutanteRepositoryImpl daoMutanteRepository;
-	
+
 	public boolean isMutant(String[] dna) {
-		
+
 		boolean flagMutant = false;
 		Integer MATCHER = 4;
-		HashBasedTable<Integer, Integer, String> adnBasedTable = HashBasedTable.create(); 
-		
+		HashBasedTable<Integer, Integer, String> adnBasedTable = HashBasedTable.create();
+
 		for (Integer indexRow = 0; indexRow < dna.length; indexRow++) {
-			
+
 			String[] dnaCadena = dna[indexRow].split("");
 			boolean flagRow = false;
-			
+
 			Integer countRow = 0;
-			for(Integer indexColumn = 0; indexColumn < dna.length; indexColumn++) {
-				
-			adnBasedTable.put(indexRow, indexColumn, dnaCadena[indexColumn]);
-				
+			for (Integer indexColumn = 0; indexColumn < dna.length; indexColumn++) {
+
+				adnBasedTable.put(indexRow, indexColumn, dnaCadena[indexColumn]);
+
 			}
-					for(Integer indexColumn = 0; indexColumn < dna.length; indexColumn++) {
-						
-						String rowString = adnBasedTable.row(indexRow).values().toString();
-						if(rowString.contains(adnBasedTable.get(indexRow, indexColumn))) {
-							countRow++;
-							if(countRow.equals(4)) {								
-								flagRow = true;							
-							}else {
-								countRow = 0;
-							}
-						}
-//				if(countRow >= MATCHER) {
-//					flagRow = true;
+			for (Integer indexColumn = 0; indexColumn < dna.length; indexColumn++) {
+
+				if (dna[indexRow].contains(adnBasedTable.get(indexRow, indexColumn).repeat(MATCHER))) {
+					flagRow = true;
+				}
+			}
+//			for (Integer indexColumn = 0; indexColumn < dna.length; indexColumn++) {
+//
+//				if (dna[indexRow].contains(adnBasedTable.get(indexRow, indexColumn).repeat(MATCHER))) {
+//					countRow++;
+//					if (countRow.equals(4)) {
+//						flagRow = true;
+//					}
+//				} else {
+//					countRow = 0;
 //				}
-			}
-//			
+//			}
+
+			
+			
 //			String[] dnaColumn = dna[indexRow+1].split("");
 //			boolean flagColumn = false;
 //				
@@ -65,13 +69,13 @@ public class MutanteService {
 //					
 //					flagMutant = true;
 //				}
-			
+
 		}
 		return flagMutant;
 	}
-	
+
 	public Mutante saveMutante(Mutante mutante) {
-		
+
 		return mutante;
 	}
 }
